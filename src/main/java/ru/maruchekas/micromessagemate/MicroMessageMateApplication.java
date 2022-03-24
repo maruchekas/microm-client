37c08453c20045c9b48c9cc93794335663739a39
maruchekas <salken84@gmail.com>
2022-03-24 10:12:13 +0300

    initial

---
+++ src/main/java/ru/maruchekas/micromessagemate/MicroMessageMateApplication.java
@@ -0,0 +1,13 @@
+package ru.maruchekas.micromessagemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroMessageMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroMessageMateApplication.class, args);
+    }
+
+}
